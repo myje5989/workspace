840cf0c4522f84a34a9bb47a83817bad67347d51
myje5989 <30823860+myje5989@users.noreply.github.com>
2018-05-24 11:30:28 +0700

    add

---
+++ Lap_5/src/BBC_Links.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BBC_Links {
+
+	public static void main(String[] args)  throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/NPRU/Desktop/Selenium ÃÒªÀÑ®¹¤Ã»°Á/Jar/BrowserDriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
+		driver.get("http://www.bbc.com/");
+		
+		WebElement table = driver.findElement(By.xpath("//*[@id='page']/section[3]/div/div/section[2]"));
+		
+		List<WebElement> list = table.findElements(By.tagName("h3"));
+		
+		System.out.println(list.size());
+		for(WebElement lst: list) {
+			
+			System.out.println(lst.getText());
+		}
+	
+		Thread.sleep(500);
+		driver.close();
+		System.out.println(" Finish !!!");
+		
+				
+	}
+	
+
+}
